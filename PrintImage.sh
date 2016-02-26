6c5d92f6dc27095e0380cb15ac7b8e299fe44b94
sheacd7 <cds5g@virginia.edu>
2016-02-26 11:04:17 -0500

    generalize awk indexing to handle arbitrary num rows per line
    pass second var to awk for number of rows in image per line - currently
    only supports 1 or 2 given limitations from full or half-block unicode
    characters
    
    pipe pixel values directly to awk instead of using second intermediate
    text file
    
    get header values without modifying IFS

--- PrintImage.sh
+++ PrintImage.sh
@@ -7,14 +7,18 @@
 # Created: 2016-02-21
 
 # TODO
+#   skip writing to intermediate txt file for image conversion
+#   preserve border (reset fg/bg to black)
+
+# DONE
 #   command line option for 1 vs 2 rows per line
 #   abstract out awk indexing
+#   skip one of two intermediate text files
 #   get rows/cols without modifying IFS
-#   skip writing to intermediate txt files
 
 imgfile="$1"
 txtfile="${imgfile%%.*}.txt"
-outfile="${imgfile%%.*}_out.txt"
+#outfile="${imgfile%%.*}_out.txt"
 
 if [[ ! -f "${txtfile}" ]]; then 
   printf '%s\n' "Converting image to text"
@@ -22,116 +26,56 @@ if [[ ! -f "${txtfile}" ]]; then
 fi
 
 printf '%s\n' "Loading image to buffer"
-old_IFS=$IFS
-IFS=':,'; read comment cols rows max format < <(head -1 "${txtfile}")
-IFS=${old_IFS}
+read -r -a header < <(head -1 "${txtfile}")
+read -r cols rows max format < <(printf '%s\n' "${header[4]//,/ }")
 printf '%s:%d, %s:%d\n' "Cols" "${cols}" "Rows" "${rows}" 
 
-# one row per line =============================================================
+# command-line arg: rows per line
+rpl=$2
+
+# map indices of pixel stream to cols * lines array
+mapfile -t img < <( \
 tail -"$((rows*cols))" "${txtfile}" | \
 cut -f 2 -d ' ' | \
 tr -d '()' | \
 tr ',' ';' | \
-awk -v c="$cols" '
+awk -v c="$cols" -v r="$rpl" '
+BEGIN {
+  row=-1
+  i=0
+}
 {
-  j=((NR-1) % c)
-  i+=(j==0)
+  col=(NR-1) % c
+  (col==0) && row++
+  (row>=r) && i++ 
+  (row>=r) && row=0
+  j=(col*r) + row 
   a[i,j]=$1
   max_NR=NR
 }
 END {
-  r=max_NR/c
-  for(i=1; i<=r; i++) {
+  rmax=max_NR/(c*r)
+  for(i=0; i<rmax; i++) {
     str=a[i,0]
-    for(j=1; j<c; j++) {
+    for(j=1; j<(c*r); j++) {
       str=str" "a[i,j]
     }
     print str
   }
-}' > "${outfile}"
+}
+' )
+#printf '%s\n' "${img[@]}"
 
 # apply terminal codes to pixel rgb values
-mapfile -t img < "${outfile}"
-for row in "${!img[@]}"; do
-  printf -v image[$row] '\033[38;2;%sm\u2588' ${img[$row]}
-done
-
-# two rows per line ============================================================
-# tail -"$((rows*cols))" "${txtfile}" | \
-# cut -f 2 -d ' ' | \
-# tr -d '()' | \
-# tr ',' ';' | \
-# awk -v c="$cols" '
-# {
-#   x=((NR-1) % (2*c))
-#   j=2*x
-#   x>=c && j-=((2*c)-1)
-#   i+=(x==0)
-#   a[i,j]=$1
-#   max_NR=NR
-# }
-# END {
-#   r=max_NR/(2*c)
-#   for(i=1; i<=r; i++) {
-#     str=a[i,0]
-#     for(j=1; j<(2*c); j++) {
-#       str=str" "a[i,j]
-#     }
-#     print str
-#   }
-# }' > "${outfile}"
-# 
-# # apply terminal codes to pixel rgb values
-# mapfile -t img < "${outfile}"
-# for row in "${!img[@]}"; do
-#   printf -v image[$row] '\033[38;2;%s;48;2;%sm\u2580' ${img[$row]}
-# done
-
+case $rpl in 
+  1) for row in "${!img[@]}"; do 
+       printf -v image[$row] '\033[38;2;%sm\u2588' ${img[$row]}; done ;;
+  2) for row in "${!img[@]}"; do 
+       printf -v image[$row] '\033[38;2;%s;48;2;%sm\u2580' ${img[$row]}; done ;;
+  *) echo "Unsupported rows per line: $rpl"; exit ;;
+esac
 
 # print image
 printf '%b\n' "${image[@]}"
 
-# scratch ======================================================================
 
-# print with full block per row
-# this method is way too slow
-#mapfile -s 1 -t pixels < <(cut -f 2 -d ' ' "${txtfile}" | tr -d '()' | tr ',' ';')
-#for ((row=0; row<${rows}; row++)); do
-#  printf -v image[$row] "\\033[38;2;%sm\\u2588" "${pixels[@]:$((row*cols)):$cols}"
-#done
-# ==============================================================================
-
-#"\\033[38;2;r;g;b;48;2;r;g;bm\\u2580"
-
-# ImageMagick pixel enumeration: 286,280,255,srgb
-#0,0: (255,255,255)  #FFFFFF  white
-#1,0: (255,255,255)  #FFFFFF  white
-
-# from hex
-#mapfile -s 1 -n $cols -O $row -t < <(cut -f 3 -d ' ' "${img}")
-# from rgb
-#mapfile -s 1 -n $cols -O $row -t < <(cut -f 2 -d ' ' "${img}" | tr ',()' ';  ' )
-
-# attempts to interleave rows (print 2 rows at once with upper block unicode) ==
-#for ((row=0; row<$(( ${rows} - 1 )); row+=2)); do
-#  printf -v image[$(($row / 2))] "\\033[38;2;%s;48;2;%sm\\u2580" \
-#    "${pixels[@]:$(( $row * $cols)):$cols}" \
-#    "${pixels[@]:$(( ($row + 1) * $cols)):$cols}"
-#done
-
-# combine alternating runs of length $cols to each row
-#for ((row=0; row<$rows; row++)); do
-#  arr[$row]=$(paste \
-#    <(sed -n $((row*10 + 1)),+4p test.txt) 
-#    <(sed -n $((row*10 + 6)),+4p test.txt) )
-#done
-#printf '%s\n' "${arr[@]}"
-
-# print with half-block per row
-#for ((row=0; row<$rows; row++)); do 
-#  row1=$((row*cols*2 +1))
-#  row2=$((row1 + cols))
-#  arr[$row]=$(paste \
-#    <(sed -n $row1,+$((cols-1))p $txtfile) \
-#    <(sed -n $row2,+$((cols-1))p $txtfile) )
-#done
